bb5f47f31633dcbb82eca09ed2f772891844a762
LightGuard <lightguard.jp@gmail.com>
2012-11-09 17:31:58 -0700

    Adding in the common things from Arquillian
    Also fixing the URLs for mailing list and issue tracker

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,5 +1,11 @@
+require File.join File.dirname(__FILE__), 'tweakruby'
+require_relative 'restclient_extensions_enabler'
+require_relative 'common' 
+require_relative 'jira'
 
 Awestruct::Extensions::Pipeline.new do
+  extension Awestruct::Extensions::RestClientExtensions::EnableGetCache.new
+  extension Awestruct::Extensions::RestClientExtensions::EnableJsonConverter.new
   extension Awestruct::Extensions::Posts.new( '/news', :posts ) 
   extension Awestruct::Extensions::Atomizer.new( :posts, '/news/news.atom' )
 
@@ -8,8 +14,9 @@ Awestruct::Extensions::Pipeline.new do
   # todo: We should probably do an extension for scraping JIRA
   #       We can do things like what BVAL is doing, but automattically
   #       based on versions, labels, etc. We'll need a rest client gem
-  
-  # todo: awestruct, as we're on a version of awestruct that I don't think
+  Awestruct::Extensions::Jira::Project.new(self, 'CDI:12311062')
+    
+  # todo: asciidoc, as we're on a version of asciidoc that I don't think
   #       has it built in we'll need our own version
 
   extension Awestruct::Extensions::Disqus.new
