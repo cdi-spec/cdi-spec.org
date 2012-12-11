06c00fe3b884a1f24a6c22db59a7c7f4f064c2ba
LightGuard <lightguard.jp@gmail.com>
2012-12-10 22:13:23 -0700

    FAQ section in place

---
+++ _ext/faq.rb
@@ -0,0 +1,39 @@
+# a small reworking of the post extension to pull out the dates and archives
+module Awestruct
+  module Extensions
+    class FAQ
+
+      attr_accessor :path_prefix, :assign_to
+
+      def initialize(path_prefix='', assign_to=:faq)
+        @path_prefix      = path_prefix
+        @assign_to        = assign_to
+      end
+
+      def execute(site)
+        faq   = []
+
+        site.pages.each do |page|
+          if ( page.relative_source_path =~ /^#{@path_prefix}\// )
+            page.relative_source_path =~ /^#{@path_prefix}\/(.*)\..*$/
+            page.slug ||= $1
+            context = page.create_context
+            page.output_path = "#{@path_prefix}/#{page.slug}.html"
+            faq << page
+          end
+        end
+
+        last = nil
+        singular = @assign_to.to_s.singularize
+        faq.each do |e|
+          if ( last != nil )
+            e.send( "next_#{singular}=", last )
+            last.send( "previous_#{singular}=", e )
+          end
+          last = e
+        end
+        site.send( "#{@assign_to}=", faq )
+      end
+    end
+  end
+end
--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -2,19 +2,21 @@ require File.join File.dirname(__FILE__), 'tweakruby'
 require_relative 'restclient_extensions_enabler'
 require_relative 'common' 
 require_relative 'jira'
+require_relative 'faq'
 
 Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::RestClientExtensions::EnableGetCache.new
   extension Awestruct::Extensions::RestClientExtensions::EnableJsonConverter.new
   extension Awestruct::Extensions::DataDir.new
-  extension Awestruct::Extensions::Posts.new( '/news', :posts ) 
-  extension Awestruct::Extensions::Atomizer.new( :posts, '/news/news.atom' )
+  extension Awestruct::Extensions::Posts.new( '/news', :news ) 
+  extension Awestruct::Extensions::FAQ.new( '/faq', :faq ) 
+  extension Awestruct::Extensions::Atomizer.new( :news, '/news/feed.atom' )
 
-  # todo: Tagging
-  
-  # todo: We should probably do an extension for scraping JIRA
-  #       We can do things like what BVAL is doing, but automattically
-  #       based on versions, labels, etc. We'll need a rest client gem
+  # It would be really cool to combine these, will need to look into it.
+  extension Awestruct::Extensions::Tagger.new( :news, '/news', '/news/tags', :per_page=>10)
+  extension Awestruct::Extensions::Tagger.new( :faq, '/faq', '/faq/tags', :per_page=>10)
+  # TODO: TagCloud
+
   Awestruct::Extensions::Jira::Project.new(self, 'CDI:12311062')
     
   # todo: asciidoc, as we're on a version of asciidoc that I don't think
