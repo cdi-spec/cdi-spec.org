fb5a9d32262cf30bd05081164ffce1d1165fa868
Antoine Sabot-Durand <antoine@sabot-durand.net>
2015-02-18 15:24:45 +0100

    Enhance blog section

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -10,12 +10,17 @@ Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::RestClientExtensions::EnableGetCache.new
   extension Awestruct::Extensions::RestClientExtensions::EnableJsonConverter.new
   extension Awestruct::Extensions::DataDir.new
-  extension Awestruct::Extensions::Posts.new( '/news', :news ) 
+  extension Awestruct::Extensions::Posts.new( '/news')
+  extension Awestruct::Extensions::Paginator.new( :posts, '/news/index', :per_page=>5 )
+  extension Awestruct::Extensions::Tagger.new( :posts, '/news/index', '/news/tags', :per_page=>5)
+  extension Awestruct::Extensions::TagCloud.new( :posts, '/news/tags/index.html', :layout=>'base' )
+  extension Awestruct::Extensions::Atomizer.new( :posts, '/news.atom', :num_entries=>20 )
+ 
+  
   extension Awestruct::Extensions::FAQ.new( '_faq', :faq ) 
-  extension Awestruct::Extensions::Atomizer.new( :news, '/news/feed.atom' )
+  # extension Awestruct::Extensions::Atomizer.new( :news, '/news/feed.atom' )
 
   # It would be really cool to combine these, will need to look into it.
-  extension Awestruct::Extensions::Tagger.new( :news, '/news', '/news/tags', :per_page=>10)
   extension Awestruct::Extensions::Tagger.new( :faq, '/faq', '/faq/tags', :per_page=>10)
   # TODO: TagCloud
 
