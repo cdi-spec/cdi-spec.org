6452a4c742e67c5fefb1f75e707a02b865474c15
Antoine Sabot-Durand <antoine@sabot-durand.net>
2015-09-29 11:26:24 +0200

    Remove jira plugin

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -2,7 +2,7 @@ require 'bootstrap-sass'
 require File.join File.dirname(__FILE__), 'tweakruby'
 require_relative 'restclient_extensions_enabler'
 require_relative 'common' 
-require_relative 'jira'
+# require_relative 'jira'
 require_relative 'faq'
 require 'awestruct/extensions/minify'
 
@@ -24,7 +24,7 @@ Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::Tagger.new( :faq, '/faq', '/faq/tags', :per_page=>10)
   # TODO: TagCloud
 
-  Awestruct::Extensions::Jira::Project.new(self, 'CDI:12311062')
+ # Awestruct::Extensions::Jira::Project.new(self, 'CDI:12311062')
     
   # todo: asciidoc, as we're on a version of asciidoc that I don't think
   #       has it built in we'll need our own version
